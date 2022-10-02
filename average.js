494eaca5b1e682ebea28652106aed94486ce7e3c
지현 <jh0neee@naver.com>
2022-10-03 00:22:45 +0900

    forEach_평균구하기

--- average.js
+++ average.js
@@ -1,11 +1,8 @@
- function solution(arr) {
-        var sum = 0;
-        for(var i=0; i<arr.length; i++) {
-            sum += arr[i];
-        }
-        var result = sum / arr.length;
-        return result;
-    }
+function solution(arr) {
+    let sum = 0; 
+    arr.forEach(n => { sum += n; });
+    return sum / arr.length;
+}
 
     console.log(solution([1,2,3,4]));
 
