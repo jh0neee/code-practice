0962f331204108626258c37b1f0920d4b4959163
지현 <jh0neee@naver.com>
2022-10-05 21:57:13 +0900

    하샤드 수

---
+++ Harshad.js
@@ -0,0 +1,10 @@
+function solution(x) {
+    let sum = 0;
+    let n = x;
+    while(n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return x % sum === 0;
+}
+console.log(solution(3274));
