6ace2d7d1e02b1754efaa98dd4f061b72f08b111
지현 <jh0neee@naver.com>
2022-10-13 20:06:21 +0900

    나누어떨어지는 수

--- divisibleNumber.js
+++ divisibleNumber.js
@@ -1,13 +1,12 @@
 function solution(arr, divisor) {
     let answer = [];
-    let minus = [-1];
     arr.map((x) => {
         if (x % divisor === 0){
             if(Number.isInteger(x)) answer.push(x);
         }
     });
-    answer.length === 0 ? answer.push(-1) : answer.sort((a,b) => {return a-b});
-    return answer;
+    return answer.length == 0 ? [-1] : answer.sort((a,b) => {return a-b});
+    
 }
 console.log(solution([3,5,2,34,19,20], 2));
 console.log(solution([3,5,19], 2));
