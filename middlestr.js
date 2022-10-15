7e3ce6acf1a4b581506666534e4a340bc7ffcd0b
지현 <jh0neee@naver.com>
2022-10-15 16:18:53 +0900

    가운데 글자 가져오기

---
+++ middlestr.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    if(s.length % 2 === 0) {
+         return s.substr(parseInt(s.length / 2) -1 , 2);
+    }
+    else{
+         return s.substr(parseInt(s.length / 2) , 1);
+    }
+}
+console.log(solution("qwerty"));
