67bb3e6906de2362347cbc51966b5496a7964da1
지현 <jh0neee@naver.com>
2022-11-02 17:09:58 +0900

    숫자 문자열과 영단어

--- numberAndstring.js
+++ numberAndstring.js
@@ -1,20 +1,13 @@
 // 숫자 문자열과 영단어
 function solution(s) {
-    const number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let result = '';
-    let alphanum = '';
-    for(let i = 0; i < s.length; i++){
-        let str_s = s[i].match(/[a-z]/gi);
-        if(str_s === null) {
-            result += s[i];
-        }else {
-            alphanum += s[i];
-            if(number.includes(alphanum)) {
-                result += number.indexOf(alphanum);
-                alphanum = "";
-            }
-        }                      
-    }
-    return Number(result);
+    const number = {
+        0 : 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 
+        5 : 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'
+    };
+        for (const [key, value] of Object.entries(number)) {
+            let regex = new RegExp(value, "g");
+	        s = s.replace(regex, key); 
+        }
+        return Number(s);
 }
 console.log(solution("one346zerofivenine"));
