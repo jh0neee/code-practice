c90b8cbbffd0627579979828d5313fc6055e926f
지현 <jh0neee@naver.com>
2022-11-09 22:33:05 +0900

    소수찾기

--- prime.js
+++ prime.js
@@ -1,16 +1,16 @@
+function isPrime(num){
+    if(num === 1) return false;
+       for(let i = 2; i <= Math.sqrt(num); i++){
+            if(num % i === 0) return false; 
+        } 
+    return true;
+}
 function solution(n) {
-    let answer = 0;
-    let notPrime = [];
-    for(let i=2; i <= n; i++){
-        for(let j=2; j < i; j++){
-            if(i%j === 0) notPrime.push(i);
-            notPrime = [...new Set(notPrime)];
-        }        
-    }
-    for(let k = 2; k <= n; k++){
-        if(!notPrime.includes(k)) answer++;
+    let answer = 0
+    for(let i = 1; i <= n; i++){
+       if(isPrime(i)) answer++;
     }
     return answer;
-
 } 
+
 console.log(solution(55));
