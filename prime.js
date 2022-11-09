e8bd339e4f83be078b46dcd8c9720889436caabf
지현 <jh0neee@naver.com>
2022-11-09 22:34:33 +0900

    소수 찾기

--- prime.js
+++ prime.js
@@ -1,8 +1,16 @@
 function isPrime(num){
-    if(num === 1) return false;
+    if(num === 1) return false; // 1은 소수 아님
+    if(num % 2 === 0) { // 짝수일 때, 2 제외 소수 아님
+        if(num === 2) return true;
+        else{
+            return false;
+        }
+    }
+    if(num % 2 !== 0){ // 홀수일 때, 제곱근보다 작은 수로 나눠지면 소수 아님
        for(let i = 2; i <= Math.sqrt(num); i++){
             if(num % i === 0) return false; 
         } 
+    }
     return true;
 }
 function solution(n) {
@@ -11,6 +19,5 @@ function solution(n) {
        if(isPrime(i)) answer++;
     }
     return answer;
-} 
-
+}
 console.log(solution(55));
