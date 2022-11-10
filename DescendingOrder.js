26ab8f63f8b45934a82a20e8540e0b2d921f843d
지현 <jh0neee@naver.com>
2022-11-10 20:29:16 +0900

    내림차순 정렬

--- DescendingOrder.js
+++ DescendingOrder.js
@@ -1,7 +1,22 @@
 function solution(n) {
-    return Number((n + '').split('')
-                          .sort()
-                          .reverse()
-                          .reduce((sum, cur) => {return sum += cur}));
+    let answer = "";
+    n = n + "";
+    let emptyArray = [];
+    for (let i = 0; i < n.length; i++) {
+        emptyArray.push(n[i]);
+    }
+    for (let j = 0; j < emptyArray.length; j++) {
+        if (emptyArray[j] < emptyArray[j+1]) {
+            let temp = emptyArray[j];
+            emptyArray[j] = emptyArray[j+1];
+            emptyArray[j+1] = temp;
+            j = -1;
+        }
+    }
+    for (let k = 0; k < emptyArray.length; k++) {
+        answer += emptyArray[k];
+    }
+    answer = Number(answer);
+    return answer;
 }
 console.log(solution(116478));
