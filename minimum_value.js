6efd484fbc3f048182b72260028373a250695a1f
지현 <jh0neee@naver.com>
2022-11-19 13:15:00 +0900

    최솟값 만들기

--- minimum_value.js
+++ minimum_value.js
@@ -1,9 +1,13 @@
 function solution(A,B){
+    // A배열 제일 큰 수 * B배열 제일 작은 수 ... A배열 제일 작은 수 * B배열 제일 큰 수
+
     let answer = 0;
 
+    // A배열 내림차순, B배열 오름차순
     let sortA = A.sort((a,b)=>b-a);
     let sortB = B.sort((a,b)=>a-b);
 
+    // 배열 길이만큼 for문 돌려서 배열 값 곱하여 return 
     for(let i = 0; i < A.length; i++) {
         answer += sortA[i] * sortB[i];
     }
